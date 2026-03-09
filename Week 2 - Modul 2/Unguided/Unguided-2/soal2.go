4ef0236dc28fb215d1485fab90b01390cbdad7aa
Zeustain <indramahottam@gmail.com>
2026-03-09 20:54:42 +0800

    penambahan File laprak modul 2

--- Week 2 - Modul 2/Unguided/Unguided-2/soal2.go
+++ Week 2 - Modul 2/Unguided/Unguided-2/soal2.go
@@ -3,25 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	var w1, w2, w3, w4 string
-	var wb1, wb2, wb3, wb4 string
+    var w1, w2, w3, w4 string
+    var wb1, wb2, wb3, wb4 string
 
-	wb1 = "merah"
-	wb2 = "kuning"
-	wb2 = "hijau"
-	wb2 = "ungu"
+    wb1 = "merah"
+    wb2 = "kuning"
+    wb3 = "hijau"
+    wb4 = "ungu"
 
-	Percobaan := 0
-	for i := 1; i <= 5; i++ {
-		fmt.Printf("Percobaan %.d : ", i)
-		fmt.Scan(&w1, &w2, &w3, &w4)
-		if w1 != wb1 || w2 != wb2 || w3 != wb3 || w4 != wb4 {
-			Percobaan++
-		}
-	}
-	if Percobaan != 0 {
-		fmt.Println("BERHASIL: true")
-	} else {
-		fmt.Println("BERHASIL: false")
-	}
+    Percobaan := 0
+    for i := 1; i <= 5; i++ {
+        fmt.Printf("Percobaan %d : ", i)
+        fmt.Scan(&w1, &w2, &w3, &w4)
+        if w1 != wb1 || w2 != wb2 || w3 != wb3 || w4 != wb4 {
+            Percobaan++
+        }
+    }
+    
+    if Percobaan != 0 {
+        fmt.Println("BERHASIL: false")
+    } else {
+        fmt.Println("BERHASIL: true")
+    }
 }
